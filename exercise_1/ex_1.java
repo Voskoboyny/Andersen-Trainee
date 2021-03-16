d0a53a4bb5bb8478a73f7e306cccaff14f291b97
Voskoboyny <59208948+Voskoboyny@users.noreply.github.com>
2021-03-16 23:12:24 +0300

    Update ex_1.java

--- exercise_1/ex_1.java
+++ exercise_1/ex_1.java
@@ -1,4 +1,15 @@
+package min;
 import java.util.Scanner;
-public class min_seven {
-    System.out.println ("hello ");
-}
+public class main {
+    public static void main(String[] args) {
+       while (true) {
+            Scanner scanner = new Scanner(System.in);
+            float s1;
+            System.out.println("Введите любое значение");
+            s1 = scanner.nextFloat();
+            if (s1 > 7) {
+                System.out.println("Привет\r\n");
+            } else
+                System.out.println(); //ну ничего страшного))
+        }
+    }
